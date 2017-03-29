a4cf19789e4ca06fbdc03310762f8c500664eb32
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-29 18:26:26 +0100

    Add mini sidebar

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
 import {Template} from 'meteor/templating';
-
 import './main.html';
+
+Meteor.startup(function () {
+});
 
 Template.Main.onRendered(function () {
     // hack to position the footer
     $(window).trigger('resize');
 });
+
+Template.Main.events({});
+
+Template.Main.helpers({});
