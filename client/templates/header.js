2d9c4056bc0d8e2e0f7d270e8adeaa8d4fc5a7e7
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-29 15:24:30 +0100

    Fix go home link

---
+++ client/templates/header.js
@@ -0,0 +1,10 @@
+import { Template } from 'meteor/templating';
+
+import './header.html';
+
+Template.Header.events({
+  'click .logo'(event) {
+    event.preventDefault();
+    Router.go("home");
+  }  
+});
