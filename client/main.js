b2c3b7022e627079f627955f221e3e2344b4514c
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-03-29 18:06:18 +0100

    Format

--- client/main.js
+++ client/main.js
@@ -1,8 +1,8 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
 import './main.html';
 
-Template.Main.onRendered(function() {
-  // hack to position the footer
-  $(window).trigger('resize');
+Template.Main.onRendered(function () {
+    // hack to position the footer
+    $(window).trigger('resize');
 });
--- client/templates/not-found.js
+++ client/templates/not-found.js
@@ -1,10 +1,10 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 
 import './not-found.html';
 
 Template.NotFound.events({
-  'click .go-home'(event) {
-    event.preventDefault();
-    Router.go("home");
-  }  
+    'click .go-home'(event) {
+        event.preventDefault();
+        Router.go("home");
+    }
 });
