52fecc6f8d3a218c6ea42a04ed8c1adef388886e
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2017-04-04 14:30:11 +0100

    breadthfirst style

--- client/templates/graph.js
+++ client/templates/graph.js
@@ -61,80 +61,22 @@ Template.Graph.onRendered(function () {
         ],
 
         layout: {
-            name: 'cose',
+            name: 'breadthfirst',
 
-            // Called on `layoutready`
-            ready: function () {
-            },
-
-            // Called on `layoutstop`
-            stop: function () {
-            },
-
-            // Whether to animate while running the layout
-            animate: true,
-
-            // The layout animates only after this many milliseconds
-            // (prevents flashing on fast runs)
-            animationThreshold: 250,
-
-            // Number of iterations between consecutive screen positions update
-            // (0 -> only updated on the end)
-            refresh: 20,
-
-            // Whether to fit the network view after when done
-            fit: true,
-
-            // Padding on fit
-            padding: 30,
-
-            // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-            boundingBox: undefined,
-
-            // Randomize the initial positions of the nodes (true) or use existing positions (false)
-            randomize: false,
-
-            // Extra spacing between components in non-compound graphs
-            componentSpacing: 100,
-
-            // Node repulsion (non overlapping) multiplier
-            nodeRepulsion: function (node) {
-                return 400000;
-            },
-
-            // Node repulsion (overlapping) multiplier
-            nodeOverlap: 10,
-
-            // Ideal edge (non nested) length
-            idealEdgeLength: function (edge) {
-                return 10;
-            },
-
-            // Divisor to compute edge forces
-            edgeElasticity: function (edge) {
-                return 100;
-            },
-
-            // Nesting factor (multiplier) to compute ideal edge length for nested edges
-            nestingFactor: 5,
-
-            // Gravity force (constant)
-            gravity: 80,
-
-            // Maximum number of iterations to perform
-            numIter: 1000,
-
-            // Initial temperature (maximum node displacement)
-            initialTemp: 200,
-
-            // Cooling factor (how the temperature is reduced between consecutive iterations
-            coolingFactor: 0.95,
-
-            // Lower temperature threshold (below this point the layout will end)
-            minTemp: 1.0,
-
-            // Whether to use threading to speed up the layout
-            useMultitasking: true
+            fit: true, // whether to fit the viewport to the graph
+            directed: false, // whether the tree is directed downwards (or edges can point in any direction if false)
+            padding: 30, // padding on fit
+            circle: false, // put depths in concentric circles if true, put depths top down if false
+            spacingFactor: 1.75, // positive spacing factor, larger => more space between nodes (N.B. n/a if causes overlap)
+            boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+            avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+            roots: undefined, // the roots of the trees
+            maximalAdjustments: 0, // how many times to try to position the nodes in a maximal way (i.e. no backtracking)
+            animate: false, // whether to transition the node positions
+            animationDuration: 500, // duration of animation in ms if enabled
+            animationEasing: undefined, // easing of animation if enabled
+            ready: undefined, // callback on layoutready
+            stop: undefined // callback on layoutstop
         }
     });
 });
